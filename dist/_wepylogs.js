f3bc6d1b36bcdefd86951fe163325cb2b69da197
siYu <sgx_sy671220@163.com>
2018-06-14 18:01:43 +0800

    add

--- dist/_wepylogs.js
+++ dist/_wepylogs.js
@@ -1,1 +1,9 @@
 console.log('WePY开启错误监控');
+console.error(`CLI报错：
+C:\\Users\\Administrator.SC-HFJS03181418\\Desktop\\wepy\\app\\src\\app.wpy
+  26:7  error  A space is required after ','  comma-spacing
+  26:8  error  ',' should be placed last      comma-style
+  26:8  error  Strings must use singlequote   quotes
+
+✖ 3 problems (3 errors, 0 warnings)
+`);
